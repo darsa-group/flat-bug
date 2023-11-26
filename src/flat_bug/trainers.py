ad3f01c35a419e3e0fd9af272fb0feb7894e3eba
Quentin Geissmann <qgeissmann@gmail.com>
2023-11-26 17:16:23 +0100

    add a callback to trainer to log learning rate

--- src/flat_bug/trainers.py
+++ src/flat_bug/trainers.py
@@ -1,16 +1,72 @@
+import torch
+from ultralytics.nn.tasks import attempt_load_one_weight
 from ultralytics.models.yolo.segment import SegmentationTrainer
 from flat_bug.datasets import MyYOLODataset, MyYOLOValidationDataset
-from ultralytics.utils import yaml_load, DEFAULT_CFG
+from ultralytics.utils import yaml_load, DEFAULT_CFG, RANK, LOGGER
+
+
+
+import math
+import os
+import subprocess
+import time
+from copy import deepcopy
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch import distributed as dist
+from torch import nn, optim
+from torch.cuda import amp
+from torch.nn.parallel import DistributedDataParallel as DDP
+from tqdm import tqdm
+
+from ultralytics.cfg import get_cfg
+from ultralytics.data.utils import check_cls_dataset, check_det_dataset
+from ultralytics.nn.tasks import attempt_load_one_weight, attempt_load_weights
+from ultralytics.utils import (DEFAULT_CFG, LOGGER, RANK, SETTINGS, TQDM_BAR_FORMAT, __version__, callbacks, clean_url,
+                               colorstr, emojis, yaml_save)
+from ultralytics.utils.autobatch import check_train_batch_size
+from ultralytics.utils.checks import check_amp, check_file, check_imgsz, print_args
+from ultralytics.utils.dist import ddp_cleanup, generate_ddp_command
+from ultralytics.utils.files import get_latest_run, increment_path
+from ultralytics.utils.torch_utils import (EarlyStopping, ModelEMA, de_parallel, init_seeds, one_cycle, select_device,
+                                           strip_optimizer)
+
+
+import inspect
 
 class MySegmentationTrainer(SegmentationTrainer):
-    def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
+    def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None, *args, **kwargs):
         """Initialize a SegmentationTrainer object with given arguments."""
         if overrides is None:
             overrides = {}
         overrides['task'] = 'segment'
 
-        super().__init__(cfg, overrides, _callbacks)
+        super().__init__(cfg, overrides, _callbacks, *args, **kwargs)
+        self.add_callback("on_train_epoch_start", MySegmentationTrainer.log_lr)
 
+
+    @staticmethod
+    def log_lr(self):
+        LOGGER.info(f"LR: {self.scheduler.get_last_lr()}")
+
+    def setup_model(self):
+        if isinstance(self.model, torch.nn.Module):  # if model is loaded beforehand. No setup needed
+            return
+
+        model, weights = self.model, None
+        ckpt = None
+        if str(model).endswith('.pt'):
+            weights, ckpt = attempt_load_one_weight(model)
+            cfg = ckpt['model'].yaml
+        else:
+            cfg = model
+        self.model = self.get_model(cfg=cfg, weights=weights, verbose=RANK == -1)  # calls Model(cfg, weights)
+        if not self.args.resume:
+            return None
+        return ckpt
     def build_dataset(self, img_path, mode='train', batch=None):
 
         if mode == "train":
