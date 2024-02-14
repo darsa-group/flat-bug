bfa9d37487d193eba90d8c42cbe07bc6d76b81fe
Quentin Geissmann <qgeissmann@gmail.com>
2024-02-14 18:45:52 +0100

    eval shel script, to include later in the readme and in pipeline

---
+++ prototypes/eval.sh
@@ -0,0 +1,5 @@
+
+mkdir /tmp/val-results
+mkdir /tmp/val-preds
+fb_predict.py -i /tmp/yolo/insects/images/val/  -w ~/Desktop/fb_2024-02-09_best.pt -o /tmp/val-preds
+fb_eval.py -g /tmp/yolo/insects/labels/val/instances_default.json -p '/tmp/val-preds/**/*.json' -I /tmp/yolo/insects/images/val -o /tmp/val-results -P
