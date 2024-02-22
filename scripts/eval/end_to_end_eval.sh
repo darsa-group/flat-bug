af1e7d957ed7e40c872972115a07248d8ea5c8a4
Quentin Geissmann <qgeissmann@gmail.com>
2024-02-22 08:39:25 +0100

    fb_* bins are in path

--- scripts/eval/end_to_end_eval.sh
+++ scripts/eval/end_to_end_eval.sh
@@ -29,12 +29,12 @@ if [[ $PWD != */flat-bug ]]; then
 fi
 
 # Prepare the environment for end-to-end evaluation
-python prototypes/prepare_environment_for_end_to_end_eval.py --dir "$DIR"
+python prepare_environment_for_end_to_end_eval.py --dir "$DIR"
 # Run the model on the validation set
-python src/bin/fb_predict.py -i "$DIR/reference/val" -w "$WEIGHTS" -o "$DIR/output" -p **.jpg -f --gpu cuda:0 --no-crops --no-overviews --verbose
+fb_predict.py -i "$DIR/reference/val" -w "$WEIGHTS" -o "$DIR/output" -p **.jpg -f --gpu cuda:0 --no-crops --no-overviews --verbose
 # Compare the predictions with the ground truth
-python src/bin/fb_eval.py -p "$DIR/output/**/**.json" -g "$DIR/reference/gt/instances_default.json" -I "$DIR/reference/val" -P -o "$DIR/eval"
+fb_eval.py -p "$DIR/output/**/**.json" -g "$DIR/reference/gt/instances_default.json" -I "$DIR/reference/val" -P -o "$DIR/eval"
 # Produce the evaluation metrics and figures
 Rscript prototypes/eval-metrics.R --input_directory "$DIR/eval" --output_directory "$DIR/results"
 # Clean up the environment
-python prototypes/clean_up_end_to_end_eval.py --dir "$DIR" --clear-all
+python ./clean_up_end_to_end_eval.py --dir "$DIR" --clear-all
