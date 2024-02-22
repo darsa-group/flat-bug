8f5d4d6f661aff0087fac9c629cc239baf33f8c3
Quentin Geissmann <qgeiss@gpu01.ghpc.au.dk>
2024-02-22 17:36:00 +0100

    working slurms

--- scripts/training/train.sh
+++ scripts/training/train.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# sbatch -p ghpc_gpu train.sh
-fb_train.py -c ./fb_config_L40S.yaml  -d flat-bug-data/yolo/
+
+#SBATCH -p ghpc_gpu                 # Name of the queue
+#SBATCH -N 1                       # Number of nodes(DO NOT CHANGE)
+#SBATCH -n 16                       # Number of CPU cores
+#SBATCH --mem=32000                 # Memory in MiB(10 GiB = 10 * 1024 MiB)
+
+ROOT=/usr/home/qgg/$USER
+source ${ROOT}/.venv/bin/activate
+fb_train.py -c ${ROOT}/flat-bug/scripts/training/fb_config_L40S.yaml  -d ${ROOT}/flat-bug-data/yolo/
