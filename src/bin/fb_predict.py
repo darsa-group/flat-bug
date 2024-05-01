4dc1548af6564df6106a4157baf966c5893d285f
asgersvenning <asgersvenning@gmail.com>
2024-05-01 12:02:42 +0200

    Various robustness improvements to inference, including much better hyperparameters and slight refactoring

--- src/bin/fb_predict.py
+++ src/bin/fb_predict.py
@@ -35,6 +35,9 @@ if __name__ == '__main__':
     args_parse.add_argument("-f", "--fast", action="store_true", help="Use fast mode.")
     args_parse.add_argument("--no-crops", action="store_true", help="Do not save the crops.")
     args_parse.add_argument("--no-overviews", action="store_true", help="Do not save the overviews.")
+    args_parse.add_argument("--no-metadata", action="store_true", help="Do not save the metadata.")
+    args_parse.add_argument("--only-overviews", action="store_true", help="Only save the overviews.")
+    args_parse.add_argument("--long-format", action="store_true", help="Use long format for storing results.")
     args_parse.add_argument("-S", "--no-save", action="store_true", help="Do not save the results.")
     args_parse.add_argument("-C", "--no-compiled-coco", action="store_true", help="Skip the production of a compiled COCO file (for all images).")
     args_parse.add_argument("--single-scale", action="store_true", help="Use single scale.")
@@ -43,23 +46,68 @@ if __name__ == '__main__':
     args = args_parse.parse_args()
     option_dict = vars(args)
 
-    assert os.path.isdir(option_dict["input_dir"])
+    print("OPTIONS:", option_dict)
+
+    isERDA = option_dict["input_dir"].startswith("erda://")
+    if isERDA:
+        from pyremotedata.implicit_mount import IOHandler, RemotePathIterator
+        print("Assuming directory exists on ERDA")
+    else:
+        _, ext = os.path.splitext(option_dict["input_dir"])
+        isVideo = ext in [".mp4", ".avi"]
+        if not isVideo:
+            assert os.path.isdir(option_dict["input_dir"])
     assert os.path.isfile(option_dict["model_weights"])
 
-    device = torch.device(option_dict["gpu"])
+    device = option_dict["gpu"]
+    if not torch.cuda.is_available() and "cuda" in device:
+        raise ValueError(f"Device(s) '{device}' is/are not available.")
+    # Detect if multi-gpu, either by comma or semicolon
+    if "," in device:
+        device = device.split(",")
+    elif ";" in device:
+        device = device.split(";")
+    if isinstance(device, list):
+        device = [f"cuda:{d}" if d.isdigit() else d for d in device]
+        device = [torch.device(d) for d in device]
+    else:
+        device = f"cuda:{device}" if device.isdigit() else device
+        device = torch.device(device)
+
     dtype = getattr(torch, option_dict["dtype"])
-    if not torch.cuda.is_available() and "cuda" in option_dict["gpu"]:
-        raise ValueError(f"Device '{option_dict['gpu']}' is not available.")
     if dtype not in [torch.float16, torch.float32, torch.bfloat16]:
         raise ValueError(f"Dtype '{option_dict['dtype']}' is not supported.")
+    
+    crops = not option_dict["no_crops"]
+    metadata = not option_dict["no_metadata"]
+    if option_dict["no_overviews"]:
+        if option_dict["only_overviews"]:
+            raise ValueError("Cannot set both --no-overviews and --only-overviews.")
+        overviews = False
+    elif option_dict["only_overviews"]:
+        if option_dict["long_format"]:
+            raise ValueError("Cannot set both --only-overviews and --long-format. --only-overviews already saves in long format (although not the same file structure as --long-format).")
+        overviews = option_dict["results_dir"]
+        crops = False
+        metadata = False
+    else:
+        overviews = True
+
+    if option_dict["long_format"]:
+        if overviews:
+            overviews = os.path.join(option_dict["results_dir"], "overviews")
+        if crops:
+            crops = os.path.join(option_dict["results_dir"], "crops")
+        if metadata:
+            metadata = os.path.join(option_dict["results_dir"], "metadata")
 
     pred = Predictor(option_dict["model_weights"], device=device, dtype=dtype)
-    pred.MIN_MAX_OBJ_SIZE = 16, 768 # Size is measured as the square root of the area
+    pred.MIN_MAX_OBJ_SIZE = 16 ** 2, 1024 ** 2 / 10 # Size is measured as the square root of the area
     pred.MAX_MASK_SIZE = 1024 # Loss of precision may occur if the mask is larger than this, but all shapes are possible. 
-    pred.SCORE_THRESHOLD = 0.05
-    pred.IOU_THRESHOLD = 0.15
-    pred.MINIMUM_TILE_OVERLAP = 384
-    pred.EDGE_CASE_MARGIN = 128 + 64
+    pred.SCORE_THRESHOLD = 0.2
+    pred.IOU_THRESHOLD = 0.2
+    pred.MINIMUM_TILE_OVERLAP = 256
+    pred.EDGE_CASE_MARGIN = 32
     pred.PREFER_POLYGONS = True # Convert masks to polygons as soon as possible, and only use the polygons for further processing - no loss of precision, but only single polygons without holes can be represented, performance impact may depend on hardware and use-case
     pred.EXPERIMENTAL_NMS_OPTIMIZATION = True
     pred.TIME = option_dict["verbose"] # Should be enabled with a verbose parameter, and maybe logged? Also not sure if it incurrs a performance penalty
@@ -72,7 +120,6 @@ if __name__ == '__main__':
     # # pred.IOU_THRESHOLD = 0.5
     # pred.PREFER_POLYGONS = True # This wasn't a hyperparameter before, but it reproduces the old behavior
 
-
     # fixme, build from pred._model!
     categories = {"id": 1, "name": "insect"}
 
@@ -83,14 +130,81 @@ if __name__ == '__main__':
         "annotations": [],
         "categories": [categories]  # Your category
     }
+    if isERDA:
+        option_dict["input_dir"] = option_dict["input_dir"].removeprefix("erda://")
+        io = IOHandler(verbose = False, clean = False)
+        io.start()
+        io.cd(option_dict["input_dir"] )
+        # Check for local file index
+        local_file_index = os.path.join(os.getcwd(), option_dict["results_dir"], f"{option_dict['input_dir'].replace('/', '_')}_file_index.txt")
+        if os.path.isfile(local_file_index):
+            with open(local_file_index, "r") as file:
+                file_index = [line.strip() for line in file.readlines()]
+            io.cache["file_index"] = file_index
 
-    files = sorted([f for f in glob.glob(os.path.join(option_dict["input_dir"], "**")) if re.search(option_dict["input_pattern"], f)])
+        file_iter = RemotePathIterator(
+            io_handler = io,
+            # These are basically network-performance parameters
+            batch_size = 64, # How many files to download at once (larger is faster, but more memory intensive)
+            batch_parallel = 10, # How many files are downloaded in parallel in during each batch (10 seems to be optimal for my connection, this is probably dependent on the amount of cores on the server)
+            max_queued_batches = 3, # This relates to how much pre-fetching is done, i.e. how many batches are queued before the download is paused. This can be as large as you want, the larger the less stuttering you will have, but requires more local *disk* (NOT RAM) space
+            n_local_files = 100 * 3 * 2, # This is parameter basically does the same as the one above, but it really needs to larger than batch_size * max_queued_batches, otherwise files will be deleted before they are used (This *will* result in an error). This parameter should probably be removed from the `pyRemoteData` package...
+            clear_local = False, # Are local files temporary? I.e. should they be deleted after use? TODO: This should also cause the previous argument to be ignored, and **never** delete files before internally
+            # These parameters are all related to file-indexing and filtering on the remote server
+            override = False, # Should the file-index be re-generated? (has to be False if store is False - otherwise an error will be thrown)
+            store = False, # This is important if we do not want to add files to the remote server (i.e. we only want to read them), if this is True, then the function will "cache" the file list in the directory in a file in the remote directory called "file_index.txt"
+            pattern = option_dict["input_pattern"] # r"^[^\/\.]+(\.jpg$|\.png$|\.jpeg$|\.JPG$|\.PNG$|\.JPEG)$", # TODO: Currently as a hack, we skip files in subdirectories i.e. files with a '/' in their name, this is not ideal, as they are still read from the remote server
+        )
+    elif isVideo:
+        import cv2
+        import tempfile
+        tmp_frame_dir = tempfile.TemporaryDirectory()
+        video_output_path = os.path.join(option_dict["results_dir"], os.path.splitext(os.path.basename(option_dict["input_dir"]))[0] + ".mp4")
+        cap = cv2.VideoCapture(option_dict["input_dir"])
+        fps = cap.get(cv2.CAP_PROP_FPS) 
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count / fps
+        video_shape = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        file_iter = []
+        # Make progress bar that shows the progress in time
+        pbar = tqdm(total=duration, desc="Reading video frames", dynamic_ncols=True, unit="s")
+        while cap.isOpened():
+            pbar.update(1/fps)
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # Write frame as file in tmp_frame_dir
+            tmp_file = os.path.join(tmp_frame_dir.name, f"{len(file_iter)}.jpg")
+            cv2.imwrite(tmp_file, frame)
+            file_iter.append(tmp_file)
+        cap.release()
+        pbar.close()
+        frames = []
+        if not option_dict["no_save"] and overviews:
+            # Create a video writer
+            if option_dict["fast"]:
+                video_shape = (video_shape[0]//2, video_shape[1]//2)
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter(video_output_path, fourcc, fps, video_shape)
+    else:
+        file_iter = sorted([f for f in glob.glob(os.path.join(option_dict["input_dir"], "**"), recursive=True) if re.search(option_dict["input_pattern"], f)])
     if option_dict["max_images"] is not None:
-        files = files[:option_dict["max_images"]]
+        if isERDA:
+            file_iter.subset(list(range(min(option_dict["max_images"], len(file_iter)))))
+        else:
+            file_iter = file_iter[:option_dict["max_images"]]
     j = 1
     all_json_results = []
-    pbar = tqdm(enumerate(files), total=len(files), desc="Processing images", dynamic_ncols=True, unit="image")
+
+    UUID = "ChangeThisTEMPORARY" # fixme, this is a temporary solution, but we should use a UUID for each run
+    
+    pbar = tqdm(enumerate(file_iter), total=len(file_iter), desc="Processing images", dynamic_ncols=True, unit="image")
     for i, f in pbar:
+        if isERDA:
+            tmp_file, file_name = f
+            f = tmp_file
+            # f = os.path.join(option_dict["results_dir"], file_name)
+            # os.rename(tmp_file, f)
         if option_dict["verbose"]:
             print(f"Processing {os.path.basename(f)}")
         pbar.set_postfix_str(f"Processing {os.path.basename(f)}")
@@ -103,28 +217,61 @@ if __name__ == '__main__':
                 result_directory = prediction.save(
                     output_directory = option_dict["results_dir"],
                     fast = option_dict["fast"],
-                    overview = not option_dict["no_overviews"],
-                    crops = not option_dict["no_crops"],
-                    mask_crops = not option_dict["fast"],
-                    identifier = "ChangeThisTEMPORARY", #str(uuid.uuid4()),
+                    overview = overviews,
+                    metadata = metadata,
+                    crops = crops,
+                    mask_crops = True,
+                    identifier = UUID, #str(uuid.uuid4()),
                 )
-                json_files = [f for f in  glob.glob(os.path.join(result_directory, "*.json"))]
-                assert len(json_files) == 1
-                all_json_results.append(json_files[0])
+                if not result_directory is None:
+                    json_files = [f for f in  glob.glob(os.path.join(result_directory, "*.json"))]
+                    assert len(json_files) == 1
+                    all_json_results.append(json_files[0])
+                if isVideo and overviews:
+                    if not result_directory is None:
+                        overview_file = glob.glob(os.path.join(result_directory, f"overview_*UUID_{UUID}.jpg"))
+                        if len(overview_file) == 1:
+                            overview_file = overview_file[0]
+                        elif len(overview_file) > 1:
+                            raise ValueError("Multiple overview files found.")
+                        elif len(overview_file) == 0:
+                            raise ValueError("No overview file found.")
+                        else:
+                            raise ValueError(f"Unexpected error. Found {len(overview_file)} overview files?")
+                    elif isinstance(overviews, str):
+                        overview_file = os.path.join(overviews, f"overview_{os.path.splitext(os.path.basename(f))[0]}_UUID_{UUID}.jpg")
+                    else:
+                        raise ValueError(f"Unexpected video inference settings. {result_directory=}, {overviews=}")
+                    frames.append(overview_file)
         except Exception as e:
             logging.error(f"Issue whilst processing {f}")
             #fixme, what is going on with /home/quentin/todo/toup/20221008_16-01-04-226084_raw_jpg.rf.0b8d397da3c47408694eeaab2cde06e5.jpg?
             logging.error(e)
             raise e
     if not option_dict["no_compiled_coco"]:
-        import json
+        if len(all_json_results) == 0:
+            print("No results found, unable to compile COCO file.")
+        else:
+            import json
 
-        compiled_coco = os.path.join(option_dict["results_dir"], "coco_instances.json")
-        pred_coco = {}
-        
-        flat_bug_predictions = [json.load(open(p)) for p in all_json_results]
-        for d in flat_bug_predictions:
-            fb_to_coco(d, pred_coco)
-        with open(compiled_coco,"w") as f:
-            json.dump(pred_coco, f)
+            compiled_coco = os.path.join(option_dict["results_dir"], "coco_instances.json")
+            pred_coco = {}
+            
+            flat_bug_predictions = [json.load(open(p)) for p in all_json_results]
+            for d in flat_bug_predictions:
+                fb_to_coco(d, pred_coco)
+            with open(compiled_coco,"w") as f:
+                json.dump(pred_coco, f)
+    if isVideo and frames:
+        for frame in tqdm(frames, desc=f"Writing video ({video_output_path})", unit="frame"):
+            img = cv2.imread(frame)
+            if option_dict["fast"]:
+                img = cv2.resize(img, (video_shape[0], video_shape[1]))
+            out.write(img)
+        out.release()
+        tmp_frame_dir.cleanup()
+    if pred._multi_gpu:
+        raise NotImplementedError("Multi-GPU support is not supported. Worker termination is not implemented.")
+    if isERDA:
+        io.stop()
 
