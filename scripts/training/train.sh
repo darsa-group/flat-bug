463d7c6e07ad0e380efd5e8620f8d07ba447c0e8
Quentin Geissmann <qgeissmann@gmail.com>
2026-06-04 17:03:07 +0200

    ibid

--- scripts/training/train.sh
+++ scripts/training/train.sh
@@ -14,11 +14,11 @@ SIZE="N"
 NAME="fb_${SIZE}_$(date +%Y-%m-%d_%H-%M-%S)"
 
 ROOT=~/Desktop/flatbug-dir/
-fb_clone_data -s ~/flat-bug/repos/scripts/training/.secrets.yaml -o ${ROOT}/flat-bug-data/pre-pro/
-fb_prepare_data -i ${ROOT}/flat-bug-data/pre-pro/  -o ${ROOT}/flat-bug-data/yolo/ -f
+#fb_clone_data -s ~/flat-bug/repos/scripts/training/.secrets.yaml -o ${ROOT}/flat-bug-data/pre-pro/
+#fb_prepare_data -i ${ROOT}/flat-bug-data/pre-pro/  -o ${ROOT}/flat-bug-data/yolo/ -f
 fb_train -c ${CONFIG} -d ${ROOT}/flat-bug-data/yolo/ --name ${NAME}
 
 
-fb_prepare_data.py -i ${ROOT}/flat-bug-data/pre-pro/  -o ${ROOT}/flat-bug-data/yolo/ -f
+#fb_prepare_data.py -i ${ROOT}/flat-bug-data/pre-pro/  -o ${ROOT}/flat-bug-data/yolo/ -f
 # fb_train.py -c ${ROOT}/scripts/training/${CONFIG} -d ${ROOT}/flat-bug-data/yolo/
-fb_train -c ${ROOT}/scripts/training/${CONFIG} -d dev/fb_yolo
+#fb_train -c ${ROOT}/scripts/training/${CONFIG} -d dev/fb_yolo
